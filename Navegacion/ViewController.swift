e88cd1253cadb4f1ee00c6837b904cc4e1826eef
Luz <ulsa@chain-chain.local>
2019-09-23 14:47:55 -0700

    Navegacion y table view por llenar

---
+++ Navegacion/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Navegacion
+//
+//  Created by Luz Arce on 9/23/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Inicio"
+        
+    }
+
+
+}
+
